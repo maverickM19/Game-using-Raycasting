270d9d3635645b32fff6e3d2ecf1a05de36a4951
Max <94763009+maverickM19@users.noreply.github.com>
2022-05-29 17:07:30 +0300

    Create general.h

---
+++ HEADERS/general.h
@@ -0,0 +1,22 @@
+#pragma once
+
+constexpr unsigned short SCREEN_HEIGHT = 768;
+constexpr unsigned short SCREEN_WIDTH = 1370;
+
+constexpr unsigned short CELL_SIZE = 64;
+constexpr unsigned short MAP_CELL_SIZE = 16;
+constexpr unsigned short MAP_HEIGHT = 12;
+constexpr unsigned short MAP_WIDTH = 16;
+
+constexpr unsigned short JUMP_HEIGHT = 200;
+const unsigned int FRAME_DURATION = 16667;
+const float PI = 3.1415926f;
+const float ONE_DEGREE_TO_RADIAN = 0.017453f;
+const float RADIAN = 57.2958f;
+
+const float ANIMATION_SPEED = 0.2f;
+
+const char PATH_MAP[] = "D:\\VisualStudioProjects\\map.txt";
+const char PATH_WALL[] = "D:\\VisualStudioProjects\\raycaster_textures\\brick.png";
+const char PATH_GUN[] = "D:\\VisualStudioProjects\\raycaster_textures\\pistol6.png";
+const char PATH_CLOUDS[] = "D:\\VisualStudioProjects\\raycaster_textures\\clouds.png";
